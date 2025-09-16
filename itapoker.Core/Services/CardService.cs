00a9ea0ec2c879e0ddb0f057bcab185c08522592
Rob Daley <robd@ita5.git>
2025-09-16 15:15:54 +0100

    Built out logic for detecting hand types

--- itapoker.Core/Services/CardService.cs
+++ itapoker.Core/Services/CardService.cs
@@ -18,8 +18,124 @@ public class CardService : ICardService
             return HandType.StraightFlush;
         else if (IsFourOfAKind(cards))
             return HandType.FourOfAKind;
+        else if (IsFullHouse(cards))
+            return HandType.FullHouse;
+        else if (IsFlush(cards))
+            return HandType.Flush;
+        else if (IsStraight(cards))
+            return HandType.Straight;
+        else if (IsThreeOfAKind(cards))
+            return HandType.ThreeOfAKind;
+        else if (IsTwoPair(cards))
+            return HandType.TwoPair;
+        else if (IsOnePair(cards))
+            return HandType.OnePair;
+        else if (IsAceHigh(cards))
+            return HandType.AceHigh;
+        else if (IsKingHigh(cards))
+            return HandType.KingHigh;
+        else if (IsQueenHigh(cards))
+            return HandType.QueenHigh;
+        else if (IsJackHigh(cards))
+            return HandType.JackHigh;
+        else if (IsTenHigh(cards))
+            return HandType.TenHigh;
+        else if (IsNineHigh(cards))
+            return HandType.NineHigh;
+        else if (IsEightHigh(cards))
+            return HandType.NineHigh;
+        else if (IsSevenHigh(cards))
+            return HandType.SevenHigh;
+        else if (IsSixHigh(cards))
+            return HandType.SixHigh;
+        else if (IsFiveHigh(cards))
+            return HandType.FiveHigh;
+        else if (IsFourHigh(cards))
+            return HandType.FourHigh;
+        else if (IsThreeHigh(cards))
+            return HandType.ThreeHigh;
+        else if (IsTwoHigh(cards))
+            return HandType.TwoHigh;
 
         return HandType.None;
+    }
+
+    private bool IsAceHigh(List<Card> cards)
+    {
+        return
+            cards.Any(x => x.Rank == CardRank.Ace);
+    }
+
+    private bool IsKingHigh(List<Card> cards)
+    {
+        return
+            cards.Any(x => x.Rank == CardRank.King);
+    }
+
+    private bool IsQueenHigh(List<Card> cards)
+    {
+        return
+            cards.Any(x => x.Rank == CardRank.Queen);
+    }
+
+    private bool IsJackHigh(List<Card> cards)
+    {
+        return
+            cards.Any(x => x.Rank == CardRank.Jack);
+    }
+
+    private bool IsTenHigh(List<Card> cards)
+    {
+        return
+            cards.Any(x => x.Rank == CardRank.Ten);
+    }
+
+    private bool IsNineHigh(List<Card> cards)
+    {
+        return
+            cards.Any(x => x.Rank == CardRank.Nine);
+    }
+
+    private bool IsEightHigh(List<Card> cards)
+    {
+        return
+            cards.Any(x => x.Rank == CardRank.Eight);
+    }
+
+    private bool IsSevenHigh(List<Card> cards)
+    {
+        return
+            cards.Any(x => x.Rank == CardRank.Seven);
+    }
+
+    private bool IsSixHigh(List<Card> cards)
+    {
+        return
+            cards.Any(x => x.Rank == CardRank.Six);
+    }
+
+    private bool IsFiveHigh(List<Card> cards)
+    {
+        return
+            cards.Any(x => x.Rank == CardRank.Five);
+    }
+
+    private bool IsFourHigh(List<Card> cards)
+    {
+        return
+            cards.Any(x => x.Rank == CardRank.Four);
+    }
+
+    private bool IsThreeHigh(List<Card> cards)
+    {
+        return
+            cards.Any(x => x.Rank == CardRank.Three);
+    }
+
+    private bool IsTwoHigh(List<Card> cards)
+    {
+        return
+            cards.Any(x => x.Rank == CardRank.Two);
     }
 
     private bool IsFourOfAKind(List<Card> cards)
@@ -29,6 +145,33 @@ public class CardService : ICardService
 
         return
             cards.GroupBy(x => x.Rank).Any(x => x.Count() == 4);
+    }
+
+    private bool IsFlush(List<Card> cards)
+    {
+        // flush
+        // five cards of the same suit
+
+        return
+            cards.GroupBy(x => x.Suit).Any(x => x.Count() == 5);
+    }
+
+    private bool IsFullHouse(List<Card> cards)
+    {
+        // full house
+        // three of a kind and one pair
+
+        return
+            IsThreeOfAKind(cards) && IsOnePair(cards);
+    }
+
+    private bool IsOnePair(List<Card> cards)
+    {
+        // one pair
+        // two cards of the same rank
+
+        return
+            cards.GroupBy(x => x.Rank).Where(x => x.Count() == 2).Count() == 1;
     }
 
     private bool IsRoyalFlush(List<Card> cards)
@@ -46,12 +189,56 @@ public class CardService : ICardService
                 x.Rank == CardRank.Ten);
     }
 
+    private bool IsStraight(List<Card> cards)
+    {
+        // straight
+        // five consecutive cards
+
+        var temp = cards.Where(x => x.Rank != CardRank.Ace)
+                        .Select(x => (int)(x.Rank))
+                        .ToList();
+
+        var aceRank = temp.Contains(2) ? 1 : 14;
+
+        foreach (var ace in cards.Where(x => x.Rank == CardRank.Ace))
+            temp.Add(aceRank);
+
+        temp = temp.OrderBy(x => x).ToList();
+
+        var differences = new List<int>();
+
+        for (var i = 0; i < temp.Count(); i++)
+            differences.Add(temp[i + 1] - temp[i]);
+
+        return
+            cards.GroupBy(x => x.Suit).Count() > 1 &&
+            differences.All(x => x == 1);
+    }
+
     private bool IsStraightFlush(List<Card> cards)
     {
         // straight flush
         // five consecutive cards of the same suit
 
         return
-            cards.Select(x => x.Suit).Distinct().Count() == 1;
+            IsFlush(cards) && IsStraight(cards);
+    }
+
+    private bool IsThreeOfAKind(List<Card> cards)
+    {
+        // three of a kind
+        // three cards of the same rank
+
+        return
+            cards.GroupBy(x => x.Rank).Any(x => x.Count() == 3);
+    }
+
+    private bool IsTwoPair(List<Card> cards)
+    {
+        // two pair
+        // two cards of the same rank
+
+        return
+            cards.GroupBy(x => x.Rank).Where(x => x.Count() == 2).Count() == 2;
     }
 }
