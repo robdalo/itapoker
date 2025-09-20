3efa6f1d62249f6ae787e1dbf53318fb7e1aab39
Rob Daley <robd@ita3.git>
2025-09-20 20:14:01 +0100

    Further improvements to UI

--- itapoker.App/itapoker/src/app/app.routes.ts
+++ itapoker.App/itapoker/src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { MainMenu } from './main-menu/main-menu';
 import { SinglePlayer } from './single-player/single-player';
 
 export const routes: Routes = [
+    { path: '', component: MainMenu },    
     { path: 'card-table', component: CardTable },
     { path: 'high-scores', component: HighScores },
-    { path: 'main-menu', component: MainMenu },
     { path: 'single-player', component: SinglePlayer },
 ];
